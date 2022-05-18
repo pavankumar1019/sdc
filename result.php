884455ec21671bbfbe7d0974bef6e158982e5bff
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-18 12:01:04 +0530

    a
    a

--- result.php
+++ result.php
@@ -20,7 +20,7 @@ if ($result->num_rows > 0) {
     echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
   }
 } else {
-  echo "0 results";
+  echo "Not Found Result Contact Office ";
 }
 $conn->close();
 ?>
