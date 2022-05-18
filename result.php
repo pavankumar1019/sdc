b11d6b053a0ec7f816c508f231569f55d045523e
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-18 12:15:24 +0530

    a

--- result.php
+++ result.php
@@ -11,16 +11,57 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+function language($code) {
+if($code=1){
+  echo "KANNADA";
+}
+if($code=2){
+  echo "ENGLISH";
+}
+if($code=3){
+  echo "HINDI";
+}
+if($code=3){
+  echo "URDU";
+}
+if($code=3){
+  echo "SANSKRIT";
+}
+}
 $sql = "SELECT * FROM bpet_ist_puc_result where reg_no='".$_POST['reg']."'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+   ?>
+   <table style="width:100%">
+  <tr>
+    <th>Register number : </th>
+    <th colspan="2">Name :</th>
+
+  </tr>
+  <tr>
+    <td colspan="3"> Father Name : <br>
+    Mother Name :
+    </td>
+   
+  </tr>
+  <tr style="text-align: center; font-size: 20px;font-weight: bold;">
+    <td><b>Subjects</b></td>
+    <td colspan="2">Marks</td>
+  </tr>
+  <tr>
+    <td ><b><?php echo language($row['lang_code'])?></b></td>
+    <td colspan="2" style="text-align: center;">40</td>
+  </tr>
+
+</table>
+   <?php
   }
 } else {
   echo "Not Found Result Contact Office ";
 }
+
 $conn->close();
 ?>
