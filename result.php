7cbdf8aff3391d89c9d9969c43175988d64e8480
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2022-05-18 12:07:30 +0530

    Update result.php

--- result.php
+++ result.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM bpet_ist_puc_result";
+$sql = "SELECT * FROM bpet_ist_puc_result where reg_no='".$_POST['reg']."'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
